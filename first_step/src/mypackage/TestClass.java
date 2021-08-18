7e5272e694baee403723d23a833082ccf6b65b69
228naruto228 <89146805+228naruto228@users.noreply.github.com>
2021-08-18 20:07:28 +0300

    Create my first program in Java

---
+++ first_step/src/mypackage/TestClass.java
@@ -0,0 +1,9 @@
+package mypackage;
+
+public class TestClass {
+
+    public static void main(String[] args) {
+        System.out.println("Hello word!");
+        System.out.println("It's my first program in Java!");
+    }
+}
