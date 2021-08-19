145539f1a214e77923dd4404026f9f9f6f59f483
228naruto228 <89146805+228naruto228@users.noreply.github.com>
2021-08-19 14:11:12 +0300

    I don't know, why codewars doesn't like my answer

---
+++ code_wars/The Feast of Many Beasts/src/mypackage/TheFeast.java
@@ -0,0 +1,15 @@
+package mypackage;
+
+public class TheFeast {
+    public static void main(String[] args) {
+        String beast = "great blue heron";
+        String dish = "garlic nann";
+
+        if (beast.charAt(0) == dish.charAt(0) && beast.charAt(beast.length() - 1) == dish.charAt(dish.length() - 1)){
+            System.out.println("true");
+        }
+        else{
+            System.out.println("false");
+        }
+    }
+}
