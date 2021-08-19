485b94af6da90d95f14a23c22ed730c1a09b7f11
228naruto228 <89146805+228naruto228@users.noreply.github.com>
2021-08-19 13:33:20 +0300

    Learn variable

--- first_step/src/mypackage/TestClass.java
+++ first_step/src/mypackage/TestClass.java
@@ -5,5 +5,30 @@ public class TestClass {
     public static void main(String[] args) {
         System.out.println("Hello word!");
         System.out.println("It's my first program in Java!");
+
+        byte a = 5; //-128 127
+        short b = -100; //-32768 32767
+        int c = 1000000;
+        long d = 1234565444;
+
+        System.out.println(a);
+        System.out.println("short b: " + b);
+
+        double double1 = 133.155;
+        System.out.println("double double1: " + double1);
+        float float1 = 150.32f;
+        System.out.println(float1);
+
+        boolean boolean1 = true;
+        boolean boolean2 = false;
+        boolean boolean3 = 1 + 2 > 0;
+
+        String str = "Hello";
+        String str2 = "World!";
+        System.out.println(str + " " + str2);
+
+        char char1 = 'q';
+        System.out.println(char1);
+
     }
 }
