e64daac5f701e91a2dfca75038c2748434dbd008
228naruto228 <89146805+228naruto228@users.noreply.github.com>
2021-08-23 13:47:43 +0300

    Learn simple operation, etc.

---
+++ simple_operation/src/mypackage/SimpleOperation.java
@@ -0,0 +1,33 @@
+package mypackage;
+
+public class SimpleOperation {
+    public static void main(String[] args) {
+
+        int a = 13;
+        int b = 5;
+        System.out.println(a / b);  // результат целое число, чтобы было с остатком надо числа с плавающей точкой.
+        System.out.println(a % 5);  // остаток от деления
+
+        System.out.println();
+        System.out.println(a > 5);
+        System.out.println(a < 5);
+        System.out.println(a >= 5);
+        System.out.println(a <= 5);
+        System.out.println(a == b);
+        System.out.println(a != b);
+
+        int i = 0;
+        i++; // i = i + 1
+        System.out.println(i);
+        i--; // i = i - 1
+        System.out.println(i);
+
+        // || - ИЛИ
+        // && - И   ВАЖНЕЕ ЧЕМ ИЛИ
+
+        a += 5; // a = a + 5
+        a -= 5; // a = a - 5
+        a *= 5; // a = a * 5
+        a /= 5; // a = a / 5
+    }
+}
